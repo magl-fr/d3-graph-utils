15b06a95abe169a62e978efd5e41526f040532f0
fanatic84 <fanatic84@gmail.com>
2017-07-30 18:37:36 +0200

    + Add unit test (if nodes are not modified during process)

--- test/graph-utils-general.test.js
+++ test/graph-utils-general.test.js
@@ -1,7 +1,7 @@
 const GraphUtils = require('../src/graph-utils');
 const GraphTestUtils = require('./graph-test-utils');
 
-test('bundle: data set diabolo', () => {
+test('removeLinksInLinks', () => {
     let data = GraphTestUtils.readFile('./test/data/data_4/data_4.json');
 
     let graphUtils = new GraphUtils(data);
@@ -11,4 +11,11 @@ test('bundle: data set diabolo', () => {
     graphUtils.removeLinksInLinks(aNode.links, {R1: true, D: true, E: true});
 
     expect(aNode.links).toEqual({ R2: true, R3: true, R4: true, R8: true });
+});
+
+test('node integrity', () => {
+    let data = GraphTestUtils.readFile('./test/data/data_4/data_4.json');
+
+    let graphUtils = new GraphUtils(data);
+    expect(graphUtils.result().nodes[0].value).toEqual(1);
 });
